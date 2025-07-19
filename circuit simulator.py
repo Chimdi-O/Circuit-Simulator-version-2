55e7c86f1de55db40b9be41aacf177555700046e
Chimdi-O <chimdioguejiofor58@example.com>
2025-07-19 02:00:20 +0100

    removed the menu bar class and instead integrated it within the app class adding the function "create" menu bar also removed the canvas class which will be added back later

--- circuit simulator.py
+++ circuit simulator.py
@@ -2,17 +2,27 @@ import customtkinter as ctk
 from CTkMenuBar import *
 import tkinter as tk
 
+class App(tk.Tk): 
+    def __init__(self): 
+        super().__init__()        
 
-class MenuBar(tk.Menu): 
-    def __init__(self, master): 
-        super().__init__(master)
+        self.title("Circuit simulator")
+        self.geometry("800x600")
         
-        self.components_menu = tk.Menu(self,tearoff = False) 
-        self.components_menu.add_cascade(label="Resistor")
-        self.components_menu.add_cascade(label="Wire")
-        self.components_menu.add_cascade(label="Battery")
-        self.components_menu.add_cascade(label="Capacitor")
-        self.components_menu.add_cascade(label="LED")
+        self.create_menubar()
+        
+        self.mouse_position = [0,self.menu_bar.winfo_height()]
+        self.mode = "none" 
+    
+    def create_menubar(self): 
+        self.menu_bar = tk.Menu(self,tearoff=False)
+         
+        self.component_menu = tk.Menu(self,tearoff = False) 
+        self.component_menu.add_cascade(label="Resistor")
+        self.component_menu.add_cascade(label="Wire")
+        self.component_menu.add_cascade(label="Battery")
+        self.component_menu.add_cascade(label="Capacitor")
+        self.component_menu.add_cascade(label="LED")
 
         self.file_menu = tk.Menu(self,tearoff=False)
         self.file_menu.add_cascade(label="Save As")  
@@ -20,37 +30,25 @@ class MenuBar(tk.Menu):
         self.file_menu.add_cascade(label="Load") 
 
 
-        self.add_cascade(label="File",menu=self.file_menu)
-        self.add_cascade(label="Edit",font=("comic sans",5))
-        self.add_cascade(label="Components",menu=self.components_menu)     
-        self.add_cascade(label="Scope")
-          
+        self.menu_bar.add_cascade(label="File",menu=self.file_menu)
+        self.menu_bar.add_cascade(label="Edit",font=("comic sans",5))
+        self.menu_bar.add_cascade(label="Components",menu=self.component_menu)     
+        self.menu_bar.add_cascade(label="Scope")
 
-
-class Canvas(tk.Canvas): 
-    def __init(self,master): 
-        super.__init__(master,fg_color="white") 
-
-
-class App(tk.Tk): 
-    def __init__(self): 
-        super().__init__()
-
-        self.title("Circuit simulator")
-        self.geometry("800x600")
-        self.menu_bar = MenuBar(master=self)
         self.config(menu=self.menu_bar)
         
-        self.Canvas = Canvas(master=self)
-        self.Canvas.pack(expand=True,fill="both") 
-        self.mouse_position = [0,self.menu_bar.winfo_height()]
-        
-
+    def create_canvas(self): 
+        pass 
+    
     def locate_mouse(self,event): 
         self.mouse_position = [event.x,event.y]
         if event.y>self.menu_bar.winfo_height():
             print(self.mouse_position)
 
+    def update(self): 
+        pass
+        #if self.mode == "drawing": 
+        
 app = App() 
 app.option_add("*Menu.Font", "utopia 205")
 app.bind('<Motion>',app.locate_mouse)
