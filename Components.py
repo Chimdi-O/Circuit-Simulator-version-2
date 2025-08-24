71e38d85d0fd2d9f5bd9705632413217047dcf54
Chimdi-O <chimdioguejiofor58@example.com>
2025-08-24 19:31:49 +0100

    -drew resistors completely within tkinter - shrunk the circles and made them white - probably some other stuff in here but I forgot ngl

--- Components.py
+++ Components.py
@@ -1,25 +1,132 @@
 import tkinter as tk
+import math 
+import numpy  
 
 
 class Wire(): 
     def __init__(self,master):
-
+        #start and end pos are updated in the event handler in main.py 
         self.start_pos = None 
         self.end_pos = None
-        self.geometry = [] 
-        self.symbol = False      
+        self.geometry = []     
+        self.circle_size = 4
 
-    def draw_wire(self,master,start_pos,end_pos): 
-        self.geometry = [master.canvas.create_line( start_pos[0],  start_pos[1], end_pos[0], end_pos[1], fill="black", width=3),
-                master.canvas.create_oval( start_pos[0] - 10  ,  start_pos[1] - 10,  start_pos[0] + 10,  start_pos[1] + 10  ,fill="blue"),
-                master.canvas.create_oval( end_pos[0] - 10  ,  end_pos[1] - 10,  end_pos[0] + 10,  end_pos[1] + 10  ,fill="blue")]
-        if self.symbol: 
-                self.geometry.append(master.canvas.create_image(( start_pos[0] +  end_pos[0])/2 ,( start_pos[1] +  end_pos[1])/2, anchor=tk.CENTER, image=self.symbol))
+    def draw_wire(self,master): 
+        self.geometry = [master.canvas.create_line( self.start_pos[0],  self.start_pos[1], self.end_pos[0], self.end_pos[1], fill="black", width=3),
+               master.canvas.create_oval( self.start_pos[0] - self.circle_size  ,  self.start_pos[1] - self.circle_size,  self.start_pos[0] + self.circle_size,  self.start_pos[1] + self.circle_size  ,fill="white"),
+              master.canvas.create_oval( self.end_pos[0] - self.circle_size  ,  self.end_pos[1] - self.circle_size,  self.end_pos[0] + self.circle_size,  self.end_pos[1] + self.circle_size  ,fill="white")]    
     
-    def move_line(self,master,start_pos,end_pos):  
-          master.canvas.coords(self.geometry[0], start_pos[0],  start_pos[1], end_pos[0], end_pos[1])
-          master.canvas.coords(self.geometry[1], start_pos[0] - 10  ,  start_pos[1] - 10,  start_pos[0] + 10,  start_pos[1] + 10)
-          master.canvas.coords(self.geometry[2], end_pos[0] - 10  ,  end_pos[1] - 10,  end_pos[0] + 10,  end_pos[1] + 10)
-          
-          if self.symbol: 
-               master.canvas.coords(self.geometry[3],( start_pos[0] +  end_pos[0])/2 ,( start_pos[1] +  end_pos[1])/2)
+    def move_line(self,master):  
+        master.canvas.coords(self.geometry[0], self.start_pos, self.end_pos)
+        master.canvas.coords(self.geometry[1], self.start_pos[0] - self.circle_size  ,  self.start_pos[1] - self.circle_size,  self.start_pos[0] + self.circle_size,  self.start_pos[1] + self.circle_size)
+        master.canvas.coords(self.geometry[2], self.end_pos[0] - self.circle_size  ,  self.end_pos[1] - self.circle_size,  self.end_pos[0] + self.circle_size,  self.end_pos[1] + self.circle_size)
+
+#Draw the resistor symbol within tkinter instead of using an image
+class Resistor(Wire): 
+     def __init__(self,master): 
+          super().__init__(master)
+          self.top_left =[]
+          self.top_right =[]
+          self.bottom_left =[]
+          self.bottom_right=[]
+          self.resistance = 0
+          self.gap_start = [] 
+          self.gap_end = []
+    
+     def draw_wire(self,master): 
+        self.top_left = self.start_pos
+        self.top_right = self.start_pos
+        self.bottom_left = self.start_pos
+        self.bottom_right= self.start_pos
+        self.gap_start = self.start_pos
+        self.gap_end = self.start_pos
+
+        self.geometry = [master.canvas.create_line( self.start_pos[0],self.start_pos[1], self.gap_start[0], self.gap_start[1], fill="black", width=3),
+               master.canvas.create_line(self.gap_end[0],self.gap_end[1],self.end_pos[0],self.end_pos[1], fill="black", width=3),
+               master.canvas.create_oval( self.start_pos[0] - self.circle_size  ,  self.start_pos[1] - self.circle_size,  self.start_pos[0] + self.circle_size,  self.start_pos[1] + self.circle_size  ,fill="white"),
+               master.canvas.create_oval( self.end_pos[0] - self.circle_size  ,  self.end_pos[1] - self.circle_size,  self.end_pos[0] + self.circle_size,  self.end_pos[1] + self.circle_size  ,fill="white"),
+
+               master.canvas.create_polygon(self.bottom_left[0],self.bottom_left[1],
+                                   self.bottom_right[0],self.bottom_right[1],
+                                   self.top_right[0],self.top_right[1],
+                                   self.top_right[0],self.top_right[1],
+                                   self.top_left[0],self.top_left[1],
+                                   fill="",outline= "black",width=3) ] 
+        # self.geometry.append(master.canvas.create_line(100,100,200,100, fill="black", width=3)) # ruler
+        
+    
+     def move_line(self,master):  
+        self.calc_shape()
+    
+        
+        master.canvas.coords(self.geometry[0], self.start_pos[0],self.start_pos[1],self.gap_start[0],self.gap_start[1])
+        master.canvas.coords(self.geometry[1], self.gap_end[0], self.gap_end[1], self.end_pos[0], self.end_pos[1])
+        master.canvas.coords(self.geometry[2], self.start_pos[0] - self.circle_size  ,  self.start_pos[1] - self.circle_size,  self.start_pos[0] + self.circle_size,  self.start_pos[1] + self.circle_size)
+        master.canvas.coords(self.geometry[3], self.end_pos[0] - self.circle_size  ,  self.end_pos[1] - self.circle_size,  self.end_pos[0] + self.circle_size,  self.end_pos[1] + self.circle_size)
+
+        master.canvas.coords(self.geometry[4],self.bottom_left[0],self.bottom_left[1],
+                                   self.bottom_right[0],self.bottom_right[1],
+                                   self.top_right[0],self.top_right[1],
+                                   self.top_right[0],self.top_right[1],
+                                   self.top_left[0],self.top_left[1])
+        
+
+
+     # use the equation of the line to find the x line what ever you call it 
+     def calc_shape(self): 
+         if self.start_pos == self.end_pos: 
+            self.top_left = self.start_pos
+            self.top_right = self.start_pos
+            self.bottom_left = self.start_pos
+            self.bottom_right= self.start_pos
+            self.gap_start = self.start_pos
+            self.gap_end = self.start_pos
+            return
+     
+         #calculates the vector starting at start pos 
+         dx = self.end_pos[0] - self.start_pos[0]
+         dy = self.end_pos[1] - self.start_pos[1]
+
+         length = math.sqrt(dx**2 + dy**2)
+
+         #normalised vectors 
+         norm_dx = dx/length
+         norm_dy = dy/length 
+
+         #normalised perpendicular vectors 
+         norm_px = -norm_dy
+         norm_py = norm_dx
+
+         #scaled normalised vectors, how far along the line + how thick the resistor should be 
+         scale = 25
+         perp_scale = 12
+
+         scaled_norm_dx = norm_dx*scale 
+         scaled_norm_dy = norm_dy*scale 
+
+         scaled_norm_px = norm_px*perp_scale
+         scaled_norm_py = norm_py*perp_scale
+
+         #middle 
+         midx = (self.end_pos[0] + self.start_pos[0])/2
+         midy = (self.end_pos[1] + self.start_pos[1])/2
+
+         mid_right = [midx - scaled_norm_dx, midy - scaled_norm_dy]
+         mid_left = [midx + scaled_norm_dx, midy + scaled_norm_dy]
+
+         self.gap_end = mid_left
+         self.gap_start = mid_right
+
+         self.top_right = [mid_right[0] + scaled_norm_px, mid_right[1] + scaled_norm_py]
+         self.bottom_right = [mid_right[0] - scaled_norm_px, mid_right[1] - scaled_norm_py]
+
+         self.top_left = [mid_left[0] + scaled_norm_px, mid_left[1] + scaled_norm_py]
+         self.bottom_left = [mid_left[0] - scaled_norm_px, mid_left[1] - scaled_norm_py]
+
+
+
+
+
+
+
+  
--- Main.py
+++ Main.py
@@ -21,24 +21,27 @@ class EventHandler():
         master.canvas.bind('<Motion>',lambda event, master=master: self.Motion_handler(event,master))
         master.canvas.bind('<Button-1>', lambda event, master=master: self.Button1_handler(event,master))
         master.canvas.bind('<ButtonRelease-1>',lambda event, master=master: self.Button1_release_handler(event,master))
+        master.canvas.bind('<Escape>',lambda event, master: self.Escape_handler(event,master))
 
     def Motion_handler(self,event,master):
         
         if self.locate_mouse: 
-            master.mouse_position = [(round(event.x/50))*50,(round(event.y/50))*50]
-            print(master.mouse_position)
+            master.mouse_position = [(round(event.x/20))*20,(round(event.y/20))*20]
+            #print(master.mouse_position)
         
         if self.move_line: 
-            self.current_component.end_pos = master.mouse_position
-            self.current_component.move_line(master,self.current_component.start_pos,self.current_component.end_pos)
+            #the y cooridinate is negative as the top of the screen is zero and as you move down the value becomes larger so for that to make mathematical sense it has to be negative
+            self.current_component.end_pos = [master.mouse_position[0],master.mouse_position[1]]
+            self.current_component.move_line(master)
 
     def Button1_handler(self,event,master): 
         print("Button 1 pressed")
         if self.start_drawing:
-             
+             master.config(cursor="cross")
+             #the y cooridinate is negative as the top of the screen is zero and as you move down the value becomes larger so for that to make mathematical sense it has to be negative
              self.current_component.start_pos = [master.mouse_position[0],master.mouse_position[1]]
              self.current_component.end_pos = self.current_component.start_pos
-             self.current_component.draw_wire(master,self.current_component.start_pos,self.current_component.end_pos)
+             self.current_component.draw_wire(master)
 
              self.move_line = True 
              self.stop_drawing = True 
@@ -53,14 +56,19 @@ class EventHandler():
 
             self.select_component(self.current_component.__class__.__name__,master)
 
-            
-             
-    
+    def Escape_handler(self,event,master): 
+        if self.start_drawing == True: 
+            self.move_line = False
+            self.stop_drawing = False 
+            self.start_drawing = False
+            master.config(cursor="arrow")
+
+
+
     def select_component(self,component,master): 
-        component_dict = {"Wire":Wire(master)}#,"Resistor", }
+        component_dict = {"Wire":Wire(master),"Resistor":Resistor(master)}
         self.current_component = component_dict[component]
         self.start_drawing = True
-         
 
 
 class App(tk.Tk): 
@@ -82,8 +90,8 @@ class App(tk.Tk):
     def create_menubar(self): 
         self.menu_bar = tk.Menu(self,tearoff=False)  
         self.component_menu = tk.Menu(self,tearoff = False) 
-        self.component_menu.add_cascade(label="Wire",command=lambda Component="Wire" ,master=self : self.EventHandler.select_component(Component,master))
-        self.component_menu.add_cascade(label="Resistor")
+        self.component_menu.add_cascade(label="Wire",command=lambda Component="Wire",master=self : self.EventHandler.select_component(Component,master))
+        self.component_menu.add_cascade(label="Resistor",command=lambda Component="Resistor",master=self : self.EventHandler.select_component(Component,master))
         self.component_menu.add_cascade(label="Battery")
         self.component_menu.add_cascade(label="Capacitor")
         self.component_menu.add_cascade(label="LED")
@@ -103,8 +111,6 @@ class App(tk.Tk):
     def create_canvas(self): 
         self.canvas = tk.Canvas(self)
         self.canvas.pack(fill="both",expand=1)  
-
-    
 
     def update(self): 
         pass    
